fb7bd3df0119e4d9d9be3c104238352c965bae64
Sachin Shah <sachinshah1999@gmail.com>
2024-04-02 18:07:34 +0530

    learn context API with themeChanger project

---
+++ themeChanger/src/contexts/Theme.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { useContext } from "react";
+
+export const ThemeContext = React.createContext({
+    themeMode: 'dark',
+    darkTheme: () => {},
+    lightTheme: () => {}
+})
+
+export const ThemeProvider = ThemeContext.Provider
+
+export default function useTheme(){
+    return useContext(ThemeContext)
+}

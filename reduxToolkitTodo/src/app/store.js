77cf00bf9727fb5938fff85d1e140b40493aff65
Sachin Shah <sachinshah1999@gmail.com>
2024-04-05 18:48:25 +0530

    add store in todo app using react-redux toolkit

---
+++ reduxToolkitTodo/src/app/store.js
@@ -0,0 +1,13 @@
+import { configureStore } from '@reduxjs/toolkit'
+import { todoReducer} from '../features/todo/todoSlice'
+
+// store 
+
+// reducer 
+
+// useSelector()        useDispatch()
+const store = configureStore({
+    reducer: todoReducer
+})
+
+export default store 

2a60f046272800e9bd14259881b3f5795c28520e
Sachin Shah <sachinshah1999@gmail.com>
2024-04-30 19:18:31 +0530

    add components

---
+++ blogAppAppwrite/src/components/PostCard.jsx
@@ -0,0 +1,24 @@
+/* eslint-disable react/prop-types */
+import { Link } from 'react-router-dom'
+import bucketService from '../appwrite/bucket'
+
+function PostCard({
+    $id,
+    title,
+    featuredImage
+}) {
+  return (
+    <Link to={`/post/${$id}`}>
+        <div className='w-full bg-gray-100 rounded-xl p-4'>
+            <div className='w-full justify-center mb-4'>
+                <img src={bucketService.getFilePreview(featuredImage)} alt={title} className='rounded-xl' />
+            </div>
+            <h2
+            className='text-xl font-bold'
+            >{title}</h2>
+        </div>
+    </Link>
+  )
+}
+
+export default PostCard
